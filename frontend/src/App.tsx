90ef676d00ed01f83d37a616788cf540390b247c
Mendestec <josemendess09@gmail.com>
2021-10-13 12:26:07 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import BarChart from 'componentes/BarChart';
+import DataTable from 'componentes/DataTable';
+import DonutChart from 'componentes/DonutChart';
+import Footer from 'componentes/Footer';
+import NavBar from 'componentes/NavBar';
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <NavBar />
+    <div className="container">
       <h1 className="text-primary">Olá mundo!</h1>
+
+      <div className="row px-3">  
+
+      <div className="col-sm-6"> 
+      <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+      <BarChart />
+      </div>
+
+      <div className="col-sm-6"> 
+      <DonutChart />
+      </div>
+
+      </div>
+      <div className="py-3"> 
+      <h2 className="text-primary">Todas as vendas</h2>
+      </div>
+      <DataTable />
     </div>
+    <Footer />
+    </>
   );
 }
 
