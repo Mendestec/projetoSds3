840ed8369fadabda419224c2a6b598a618d5090a
Mendestec <josemendess09@gmail.com>
2021-10-18 17:34:29 -0300

    BarChart integration

--- frontend/src/componentes/BarChart/index.tsx
+++ frontend/src/componentes/BarChart/index.tsx
@@ -1,4 +1,76 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Chart from 'react-apexcharts';
+import { SaleSuccess } from 'types/sale';
+import { round } from 'utils/formato';
+import { BASE_URL } from 'utils/requests';
 
+type SeriesData = {
+    name: string;
+    data: number[];
+}
+
+type ChartData = {
+    labels: {
+        categories: string[];
+    };
+    series: SeriesData[];
+}
+
+const BarChart = () => {
+    
+    const [chartData, setChartData] = useState<ChartData>({
+        labels: {
+            categories: []
+        },
+        series:[{
+            name: "",
+            data: []
+        }]
+    });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/success-by-seller`)
+            .then(Response => {
+                const data = Response.data as SaleSuccess[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => round(100.0 * x.deals / x.visited, 1));
+
+                setChartData({ 
+                    labels: {
+                        categories: myLabels
+                    },
+                    series:[{
+                        name: "% Success",
+                        data: mySeries
+                    }]
+                 });
+            });
+
+    }, []);
+
+    const options = {
+        plotOptions: {
+            bar: {
+                horizontal: true,
+            }
+        },
+    };
+    
+    return (
+        <Chart 
+        options={{...options, xaxis: chartData.labels}}
+        series={chartData.series}
+        type="bar"
+        height="240"
+        />
+    );
+
+}
+
+export default BarChart;
+
+/*
 import Chart from 'react-apexcharts';
 
 const BarChart = () => {
@@ -35,3 +107,4 @@ const BarChart = () => {
 }
 
 export default BarChart;
+*/
